144d7463267fcb85cc2e2b414a76492550b3d753
dadaisma <infodadaisma@gmail.com>
2023-03-19 18:36:57 +0100

    authdone

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,48 +2,51 @@ import Input from "@/components/Input";
 import axios from "axios";
 import { useCallback, useState } from "react";
 import {signIn} from 'next-auth/react';
-import { useRouter } from "next/router";
+
+import {FcGoogle} from 'react-icons/fc';
+import {FaGithub} from 'react-icons/fa';
 
 
 const Auth = () => {
-    const router = useRouter();
-    const [email, setEmail] =useState('');
-    const [name, setName] =useState('');
-    const [password, setPassword] =useState('');
-
-    const[variant, setVariant]=useState('login')
-    const toggleVariant = useCallback(()=>{
-        setVariant((currentVariant)=>currentVariant ==='login' ? 'register': 'login')
-    },[])
-
-    const login = useCallback(async()=>{
-        try{
-            await signIn('credentials',{
-                email,
-                password,
-                redirect:false,
-                callbackUrl:'/'
-            });
-
-            router.push('/');
-        } catch (error){
-            console.log(error)
-        }
-    },[email, password, router])
-
-    const register = useCallback(async()=>{
-       try{
-        await axios.post('/api/register',{
-            email,
-            name,
-            password
-        })
+   
+  
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const [variant, setVariant] = useState('login');
+  
+    const toggleVariant = useCallback(() => {
+      setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+    }, []);
+  
+    const login = useCallback(async () => {
+      try {
+        await signIn('credentials', {
+          email,
+          password,
+          callbackUrl: '/profiles'
+        });
+  
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }, [email, password]);
+  
+    const register = useCallback(async () => {
+      try {
+        await axios.post('/api/register', {
+          email,
+          name,
+          password
+        });
+  
         login();
-       } catch (error){
-        console.log(error)
-       }
-    },[email,name,password, login])
-
+      } catch (error) {
+          console.log(error);
+      }
+    }, [email, name, password, login]);
     
 
     return (
@@ -89,6 +92,37 @@ const Auth = () => {
            ">
             {variant === 'login' ? 'login' : 'Sign up'}
            </button>
+                <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                <div onClick={()=>signIn('google',{callbackUrl:'/profiles'} )} className="
+                w-10
+                h-10
+                bg-white
+                rounded-full
+                flex
+                items-center
+                justify-center
+                cursor-pointer
+                hover:opacity-80
+                transition
+                ">
+                    <FcGoogle size={30}/>
+                </div>
+                <div onClick={()=>signIn('github',{callbackUrl:'/profiles'} )} className="
+                w-10
+                h-10
+                bg-white
+                rounded-full
+                flex
+                items-center
+                justify-center
+                cursor-pointer
+                hover:opacity-80
+                transition
+                ">
+                    <FaGithub size={30}/>
+                </div>
+                </div>
+
            <p className="text-neutral-500 mt-12">
             {variant === 'login' ?  'First time using MovieTime?' :'Already have an account?' }
             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,32 @@
+import useCurrentUser from '@/hooks/userCurrentUser';
+import { NextPageContext } from 'next';
+import {getSession, signOut} from 'next-auth/react';
 
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
+  const {data: user} =useCurrentUser();
   return (
     <>
      <h1 className="text-2xl text-green-500">MovieTown2</h1>
+     <p className='text-white'>Logged in as: {user?.name} </p>
+     <button onClick={()=>signOut()} className='h-10 w-full bg-white' >Logout</button>
     </>
   )
 }
