aa5457f8d3c38bd2753a700ca2ea27e7c74b4193
dadaisma <infodadaisma@gmail.com>
2023-03-21 18:31:05 +0100

    pics

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -50,12 +50,12 @@ const Auth = () => {
     
 
     return (
-     <div className="relative h-full w-full bg-[url('/images/hero.jpg')]
+     <div className="relative h-full w-full bg-[url('/images/cinema-day-young-couple-with-popcorn-looking-action-movie-cinema.jpg')]
       bg-no-repeat bg-center bg-fixed bg-cover">
 
 <div className="bg-black w-full h-full lg:bg-opacity-50">
             <nav className="px-12 py-5">
-                <img src="/images/logo.png" className="h-12" alt="logo" />
+                <img src="/images/Mt Logo - 2000x795px.png" className="h-12" alt="logo" />
             </nav>
             <div className="flex justify-center">
                 <div className="bg-black bg-opacity-70 px16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
