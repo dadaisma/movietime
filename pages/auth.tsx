15aca0b6ea279bc9d70bd623292fb260a2f9fbfc
dadaisma <infodadaisma@gmail.com>
2023-03-31 13:07:46 +0200

    d

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,8 +3,8 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import {signIn} from 'next-auth/react';
 
-//import {FcGoogle} from 'react-icons/fc';
-import {FaGithub} from 'react-icons/fa';
+import {FcGoogle} from 'react-icons/fc';
+import {FaGithub, FaGoogle} from 'react-icons/fa';
 
 
 const Auth = () => {
@@ -92,8 +92,24 @@ const Auth = () => {
            ">
             {variant === 'login' ? 'login' : 'Sign up'}
            </button>
-                <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+          
                 
+               
+                <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                <div onClick={()=>signIn('google',{callbackUrl:'/profiles'} )} className="
+                w-10
+                h-10
+                bg-white
+                rounded-full
+                flex
+                items-center
+                justify-center
+                cursor-pointer
+                hover:opacity-80
+                transition
+                ">
+                    <FcGoogle size={30}/>
+                </div>
                 <div onClick={()=>signIn('github',{callbackUrl:'/profiles'} )} className="
                 w-10
                 h-10
